3f7e22955053b0b46869ab7bdff2ae6f36fa9c67
rintosaputro <saputrorinto@gmail.com>
2023-05-11 18:05:04 +0700

    feat: thumbs down component

---
+++ src/components/ThumbsDown.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  HiOutlineThumbUp, HiOutlineThumbDown, HiThumbUp, HiThumbDown,
+} from 'react-icons/hi';
+
+function ThumbsDown() {
+  return (
+
+    <div>
+      <HiOutlineThumbDown />
+      <span>1</span>
+    </div>
+  );
+}
+
+export default ThumbsDown;
