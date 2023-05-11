a4968beb9e4884eccf2f776d6ce97f25babb75ee
rintosaputro <saputrorinto@gmail.com>
2023-05-11 18:04:26 +0700

    feat: button component

--- src/components/ButtonBase.jsx
+++ src/components/ButtonBase.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ButtonBase({
-  type = 'button', variant = 'primary', children, onClick,
+  type = 'button', variant = 'primary', children, onClick, size = 'btn-normal',
 }) {
+  let classVariant = 'btn-unset';
+  if (variant === 'primary') {
+    classVariant = 'btn-primary';
+  }
+  if (variant === 'secondary') {
+    classVariant = 'btn-secondary';
+  }
+
   return (
-    <button type={type} title={children} onClick={onClick} className={variant === 'primary' ? 'btn-primary' : 'btn-secondary'}>{children}</button>
+    <button
+      type={type}
+      title={children}
+      onClick={onClick}
+      className={`${classVariant} ${size}`}
+    >
+      {children}
+    </button>
   );
 }
 
 ButtonBase.propTypes = {
   type: PropTypes.oneOf(['button', 'submit']),
-  variant: PropTypes.oneOf(['primary', 'secondary']),
+  variant: PropTypes.oneOf(['primary', 'secondary', 'unset']),
   children: PropTypes.string,
   onClick: PropTypes.func.isRequired,
+  size: PropTypes.oneOf(['btn-normal', 'btn-small']),
 };
 
 export default ButtonBase;
