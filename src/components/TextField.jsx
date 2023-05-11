13dd481a81351a8310cabcf9fdb10c1d81dafff1
rintosaputro <saputrorinto@gmail.com>
2023-05-11 15:28:40 +0700

    feat: text field component

---
+++ src/components/TextField.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TextField({
+  type = 'text', value, onChange, placeholder = '',
+}) {
+  return (
+    <input type={type} value={value} onChange={onChange} placeholder={placeholder} />
+  );
+}
+
+TextField.propTypes = {
+  type: PropTypes.oneOf(['text', 'email', 'password']),
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+
+};
+
+export default TextField;
