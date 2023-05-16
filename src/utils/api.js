31f89d935e5ce8730ecbf38b040743bde7a0c78a
rintosaputro <saputrorinto@gmail.com>
2023-05-16 09:47:04 +0700

    feat: api auth

--- src/utils/api.js
+++ src/utils/api.js
@@ -16,7 +16,43 @@ const api = (() => {
   const register = async ({ name, email, password }) => {
     const response = await fetch(`${BASE_URL}/register`, {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name, email, password,
+      }),
     });
+
+    const responseJson = await response.json();
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    const { data: { user } } = responseJson;
+    return user;
+  };
+
+  const login = async ({ email, password }) => {
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const responseJson = await response.json();
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    const { data: { token } } = responseJson;
+    return token;
   };
 })();
 
