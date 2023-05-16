0d7150890ebb33ff71199f08312c4116b0adc443
rintosaputro <saputrorinto@gmail.com>
2023-05-16 10:19:42 +0700

    feat: register integration

--- src/components/RegisterInput.jsx
+++ src/components/RegisterInput.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TextField from './TextField';
 import useInput from '../hooks/useInput';
 import ButtonBase from './ButtonBase';
 
-function RegisterInput() {
+function RegisterInput({ onRegister }) {
   const [name, handleName] = useInput('');
   const [email, handleEmail] = useInput('');
   const [password, handlePassword] = useInput('');
@@ -13,9 +14,13 @@ function RegisterInput() {
       <TextField type="text" value={name} onChange={handleName} placeholder="Name" />
       <TextField type="email" value={email} onChange={handleEmail} placeholder="Email" />
       <TextField type="password" value={password} onChange={handlePassword} placeholder="Password" />
-      <ButtonBase type="submit" onClick={() => null}>Register</ButtonBase>
+      <ButtonBase type="submit" onClick={() => onRegister({ name, email, password })}>Register</ButtonBase>
     </form>
   );
 }
+
+RegisterInput.propTypes = {
+  onRegister: PropTypes.func.isRequired,
+};
 
 export default RegisterInput;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import RegisterInput from '../components/RegisterInput';
+import { asyncRegisterUser } from '../states/users/action';
 
 function RegisterPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onRegister = ({ name, email, password }) => {
+    dispatch(asyncRegisterUser({ name, email, password }));
+    navigate('/');
+  };
+
   return (
     <section className="register-page">
       <div className="auth-overlay">
@@ -10,7 +20,7 @@ function RegisterPage() {
           REGISTER TO ACCESS FORUM APP
         </header>
         <article>
-          <RegisterInput />
+          <RegisterInput onRegister={onRegister} />
           <div className="option-link">
             <span>Have an account?</span>
             <Link to="/">
