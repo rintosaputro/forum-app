f5e7fca93ec923197576e199c781341cd1655934
rintosaputro <saputrorinto@gmail.com>
2023-05-20 22:17:39 +0700

    feat: reducer unlike thread

--- src/states/threads/reducer.js
+++ src/states/threads/reducer.js
@@ -7,15 +7,21 @@ const threadsReducer = (threads = [], action = {}) => {
     case ActionType.TOGGLE_LIKE_THREAD:
       return threads.map((thread) => {
         if (thread.id === action.payload.threadId) {
-          // return {
-          //   ...thread,
-          //   upVotesBy: thread.upVotesBy.includes(action.payload.userId)
-          //     ? thread.upVotesBy.filter((voter) => voter !== action.payload.userId)
-          //     : thread.upVotesBy.concat([action.payload.userId]),
-          // };
           return {
             ...thread,
             upVotesBy: thread.upVotesBy.concat([action.payload.userId]),
+            downVotesBy: thread.downVotesBy.filter((voter) => voter !== action.payload.userId),
+          };
+        }
+        return thread;
+      });
+    case ActionType.TOGGLE_UNLIKE_THREAD:
+      return threads.map((thread) => {
+        if (thread.id === action.payload.threadId) {
+          return {
+            ...thread,
+            upVotesBy: thread.upVotesBy.filter((voter) => voter !== action.payload.userId),
+            downVotesBy: thread.downVotesBy.concat([action.payload.userId]),
           };
         }
         return thread;
@@ -26,6 +32,7 @@ const threadsReducer = (threads = [], action = {}) => {
           return {
             ...thread,
             upVotesBy: thread.upVotesBy.filter((voter) => voter !== action.payload.userId),
+            downVotesBy: thread.downVotesBy.filter((voter) => voter !== action.payload.userId),
           };
         }
         return thread;
