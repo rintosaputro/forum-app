4202f844da4f64fcb54626486ae360285083353d
rintosaputro <saputrorinto@gmail.com>
2023-05-20 22:17:24 +0700

    feat: action un like thread

--- src/states/threads/action.js
+++ src/states/threads/action.js
@@ -3,6 +3,7 @@ import api from '../../utils/api';
 const ActionType = {
   RECEIVE_THREADS: 'RECEIVE_THREADS',
   TOGGLE_LIKE_THREAD: 'TOGGLE_LIKE_THREAD',
+  TOGGLE_UNLIKE_THREAD: 'TOGGLE_UNLIKE_THREAD',
   TOGGLE_NEUTRAL_THREAD: 'TOGGLE_NEUTRAL_THREAD',
 };
 
@@ -15,6 +16,13 @@ const receiveThreadsActionCreator = (threads) => ({
 
 const toggleLikeThreadActionCreator = ({ threadId, userId }) => ({
   type: ActionType.TOGGLE_LIKE_THREAD,
+  payload: {
+    threadId, userId,
+  },
+});
+
+const toggleUnLikeThreadActionCreator = ({ threadId, userId }) => ({
+  type: ActionType.TOGGLE_UNLIKE_THREAD,
   payload: {
     threadId, userId,
   },
@@ -47,6 +55,17 @@ const asyncToggleLikeThread = (threadId) => async (dispatch, getState) => {
   }
 };
 
+const asyncToggleUnLikeThread = (threadId) => async (dispatch, getState) => {
+  const { authUser } = getState();
+  dispatch(toggleUnLikeThreadActionCreator({ threadId, userId: authUser.id }));
+  try {
+    await api.toggleLikeThread(threadId);
+  } catch (err) {
+    alert(err.message);
+    dispatch(toggleUnLikeThreadActionCreator({ threadId, userId: authUser.id }));
+  }
+};
+
 const asyncToggleNeutralThread = (threadId) => async (dispatch, getState) => {
   const { authUser } = getState();
   dispatch(toggleNeutralThreadActionCreator({ threadId, userId: authUser.id }));
@@ -63,5 +82,6 @@ export {
   receiveThreadsActionCreator,
   asyncReceiveThreads,
   asyncToggleLikeThread,
+  asyncToggleUnLikeThread,
   asyncToggleNeutralThread,
 };
