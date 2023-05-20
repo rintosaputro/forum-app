b425cbe0e8617c65f2cf1cd7c2ed17ebdb99121a
rintosaputro <saputrorinto@gmail.com>
2023-05-20 14:46:44 +0700

    feat: threads action state

---
+++ src/states/threads/action.js
@@ -0,0 +1,26 @@
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_THREADS: 'RECEIVE_THREADS',
+};
+
+const receiveThreadsActionCreator = (threads) => ({
+  type: ActionType.RECEIVE_THREADS,
+  payload: {
+    threads,
+  },
+});
+
+const asyncReceiveThreads = () => async (dispatch) => {
+  try {
+    const threads = await api.getAllThreads();
+    dispatch(receiveThreadsActionCreator(threads));
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export {
+  ActionType,
+  asyncReceiveThreads,
+};
