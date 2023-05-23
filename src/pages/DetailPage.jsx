c636c0a07a5ea4078f285555a8dfa8e9f5bd16e3
rintosaputro <saputrorinto@gmail.com>
2023-05-23 15:25:09 +0700

    feat: implementation component

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
 import ThreadDetail from '../components/ThreadDetail';
+import ReplyThreadInput from '../components/ReplyThreadInput';
+import RepliesList from '../components/RepliesList';
 
 function DetailPage() {
   const empty = 0;
+
+  const onReply = (value) => {
+    alert(value);
+  };
 
   return (
     <section className="detail-page">
@@ -11,6 +17,8 @@ function DetailPage() {
         <div className={empty === 0 ? 'empty-content' : ''}>
           <Header />
           <ThreadDetail />
+          <ReplyThreadInput onReply={onReply} />
+          <RepliesList />
         </div>
       </div>
     </section>
