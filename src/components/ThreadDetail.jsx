c511a75ba29b29f767e5bcca84d0c5ce013eadf0
rintosaputro <saputrorinto@gmail.com>
2023-05-23 15:24:50 +0700

    feat: completing thread detail

--- src/components/ThreadDetail.jsx
+++ src/components/ThreadDetail.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { postDateFormat } from '../utils';
+import Badge from './Badge';
+import ThumbsUp from './ThumbsUp';
+import ThumbsDown from './ThumbsDown';
 
 function ThreadDetail() {
   return (
@@ -11,11 +14,26 @@ function ThreadDetail() {
         </h2>
         <span className="time-thread">{postDateFormat(new Date())}</span>
       </div>
-      <h3 className="title-detail">Rinto</h3>
+      <h3 className="title-detail m-title">Rinto</h3>
       <div className="description-detail">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam magni
         reiciendis iste ipsum maxime temporibus ullam rerum veritatis.
         Libero esse fugit eius modi repellat quis corrupti rem quaerat perspiciatis tenetur?
+      </div>
+      <div className="badge-detail">
+        <Badge size="big">#Category</Badge>
+      </div>
+      <div className="thumbs-container">
+        <ThumbsUp
+          totalThumbs={2}
+          isActive
+          onLike={() => null}
+        />
+        <ThumbsDown
+          totalThumbs={2}
+          isActive
+          onUnLike={() => null}
+        />
       </div>
     </div>
   );
