e3f6a52eaf3f9a5da4646d5579f6b674ed633812
rintosaputro <saputrorinto@gmail.com>
2023-06-12 13:47:26 +0700

    feat: like, unlike, neutral thread detail func redux

--- src/states/threadDetail/action.js
+++ src/states/threadDetail/action.js
@@ -4,6 +4,9 @@ import api from '../../utils/api';
 const ActionType = {
   RECEIVE_THREAD_DETAIL: 'RECEIVE_THREAD_DETAIL',
   CLEAR_THREAD_DETAIL: 'CLEAR_THREAD_DETAIL',
+  TOGGLE_LIKE_THREAD_DETAIL: 'TOGGLE_LIKE_THREAD_DETAIL',
+  TOGGLE_UNLIKE_THREAD_DETAIL: 'TOGGLE_UNLIKE_THREAD_DETAIL',
+  TOGGLE_NEUTRAL_THREAD_DETAIL: 'TOGGLE_NEUTRAL_THREAD_DETAIL',
   TOGGLE_LIKE_COMMENT: 'TOGGLE_LIKE_COMMENT',
   TOGGLE_UNLIKE_COMMENT: 'TOGGLE_UNLIKE_COMMENT',
   TOGGLE_NEUTRAL_COMMENT: 'TOGGLE_NEUTRAL_COMMENT',
@@ -16,6 +19,21 @@ const receiveThreadDetailActionCreator = (threadDetail) => ({
 
 const clearThreadDetailActionCreator = () => ({
   type: ActionType.CLEAR_THREAD_DETAIL,
+});
+
+const toggleLikeThreadDetailActionCreator = (userId) => ({
+  type: ActionType.TOGGLE_LIKE_THREAD_DETAIL,
+  payload: { userId },
+});
+
+const toggleUnlikeThreadDetailActionCreator = (userId) => ({
+  type: ActionType.TOGGLE_UNLIKE_THREAD_DETAIL,
+  payload: { userId },
+});
+
+const toggleNeutralThreadDetailActionCreator = (userId) => ({
+  type: ActionType.TOGGLE_NEUTRAL_THREAD_DETAIL,
+  payload: { userId },
 });
 
 const asyncReceiveThreadDetail = (threadId) => async (dispatch) => {
@@ -31,7 +49,56 @@ const asyncReceiveThreadDetail = (threadId) => async (dispatch) => {
   dispatch(hideLoading());
 };
 
+const asyncToggleLikeThreadDetail = () => async (dispatch, getState) => {
+  dispatch(showLoading());
+  const { authUser, threadDetail } = getState();
+  dispatch(toggleLikeThreadDetailActionCreator(authUser.id));
+
+  try {
+    await api.toggleLikeThread(threadDetail.id);
+  } catch (err) {
+    alert(err.message);
+  }
+
+  dispatch(hideLoading());
+};
+
+const asyncToggleUnlikeThreadDetail = () => async (dispatch, getState) => {
+  dispatch(showLoading());
+  const { authUser, threadDetail } = getState();
+  dispatch(toggleUnlikeThreadDetailActionCreator(authUser.id));
+
+  try {
+    await api.toggleUnLikeThread(threadDetail.id);
+  } catch (err) {
+    alert(err.message);
+  }
+
+  dispatch(hideLoading());
+};
+
+const asyncToggleNeutralThreadDetail = () => async (dispatch, getState) => {
+  dispatch(showLoading());
+  const { authUser, threadDetail } = getState();
+  dispatch(toggleNeutralThreadDetailActionCreator(authUser.id));
+
+  try {
+    await api.toggleNeutralThread(threadDetail.id);
+  } catch (err) {
+    alert(err.message);
+  }
+
+  dispatch(hideLoading());
+};
+
 export {
   ActionType,
+  receiveThreadDetailActionCreator,
+  clearThreadDetailActionCreator,
+  toggleLikeThreadDetailActionCreator,
+  toggleNeutralThreadDetailActionCreator,
   asyncReceiveThreadDetail,
+  asyncToggleLikeThreadDetail,
+  asyncToggleUnlikeThreadDetail,
+  asyncToggleNeutralThreadDetail,
 };
--- src/states/threadDetail/reducer.js
+++ src/states/threadDetail/reducer.js
@@ -6,6 +6,25 @@ const threadDetailReducer = (threadDetail = null, action = {}) => {
       return action.payload.threadDetail;
     case ActionType.CLEAR_THREAD_DETAIL:
       return null;
+    case ActionType.TOGGLE_LIKE_THREAD_DETAIL:
+      return {
+        ...threadDetail,
+        upVotesBy: threadDetail.upVotesBy.concat([action.payload.userId]),
+        downVotesBy: threadDetail.downVotesBy.filter((vote) => vote !== action.payload.userId),
+      };
+    case ActionType.TOGGLE_UNLIKE_THREAD_DETAIL:
+      return {
+        ...threadDetail,
+        upVotesBy: threadDetail.upVotesBy.filter((vote) => vote !== action.payload.userId),
+        downVotesBy: threadDetail.downVotesBy.concat([action.payload.userId]),
+      };
+    case ActionType.TOGGLE_NEUTRAL_THREAD_DETAIL:
+      return {
+        ...threadDetail,
+        upVotesBy: threadDetail.upVotesBy.filter((upVote) => upVote !== action.payload.userId),
+        downVotesBy:
+          threadDetail.downVotesBy.filter((downVote) => downVote !== action.payload.userId),
+      };
     default:
       return threadDetail;
   }
