f46156d5c918e340a00ad1191614dea057f0fe70
rintosaputro <saputrorinto@gmail.com>
2023-06-20 13:43:26 +0700

    feat: test TOGGLE_UNLIKE_COMMENT

--- src/states/threadDetail/reducer.test.js
+++ src/states/threadDetail/reducer.test.js
@@ -273,7 +273,7 @@ describe('threadDetailReducer function', () => {
             avatar: 'https://ui-avatars.com/api/?name=Dimas Saputra&background=random',
           },
           upVotesBy: [],
-          downVotesBy: [],
+          downVotesBy: ['user-test'],
         },
       ],
       upVotesBy: ['user-3'],
@@ -295,6 +295,58 @@ describe('threadDetailReducer function', () => {
         {
           ...initialState.comments[0],
           upVotesBy: [action.payload.userId],
+          downVotesBy: [],
+        },
+      ],
+    });
+  });
+
+  it('should return the thread detail with the toggled unlike comment in thread detail when given by TOGGLE_UNLIKE_COMMENT action', () => {
+    const initialState = {
+      id: 'thread-1',
+      title: 'Halo! ',
+      body: 'Bagaimana kabarmu? Semoga baik-baik saja ya.Sekali lagi saya ucapkan selamat datang semuanya!',
+      createdAt: '2023-05-29T07:54:35.746Z',
+      owner: {
+        id: 'user-1',
+        name: 'Dicoding',
+        avatar: 'https://ui-avatars.com/api/?name=Dicoding&background=random',
+      },
+      category: 'perkenalan',
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Halo!<br>Perkanalkan, nama saya Dimas.',
+          createdAt: '2023-05-29T07:59:04.689Z',
+          owner: {
+            id: 'user-2',
+            name: 'Dimas Saputra',
+            avatar: 'https://ui-avatars.com/api/?name=Dimas Saputra&background=random',
+          },
+          upVotesBy: ['user-test'],
+          downVotesBy: [],
+        },
+      ],
+      upVotesBy: ['user-3'],
+      downVotesBy: [],
+    };
+    const action = {
+      type: 'TOGGLE_UNLIKE_COMMENT',
+      payload: {
+        commentId: 'comment-1',
+        userId: 'user-test',
+      },
+    };
+
+    const nextState = threadDetailReducer(initialState, action);
+
+    expect(nextState).toEqual({
+      ...initialState,
+      comments: [
+        {
+          ...initialState.comments[0],
+          upVotesBy: [],
+          downVotesBy: [action.payload.userId],
         },
       ],
     });
