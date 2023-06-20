02c1ef65b2b4ee28db6453dabdb1123d7677a8fc
rintosaputro <saputrorinto@gmail.com>
2023-06-20 11:30:14 +0700

    feat: test TOGGLE_NEUTRAL_THREAD

--- src/states/threads/reducer.test.js
+++ src/states/threads/reducer.test.js
@@ -151,4 +151,56 @@ describe('threadsReducer function', () => {
       },
     ]);
   });
+
+  it('should return the threads with the toggled neutral thread when given by TOGGLE_NEUTRAL_THREAD action', () => {
+    const initialState = [
+      {
+        id: 'thread-1',
+        title: 'Next js',
+        body: 'Server side rendering di dalam next js',
+        category: 'ssr',
+        createdAt: '2023-06-20T03:26:03.333Z',
+        ownerId: 'user-1',
+        totalComments: 1,
+        upVotesBy: ['user-1'],
+        downVotesBy: ['user-2'],
+      },
+    ];
+    // action for neutral like
+    const action = {
+      type: 'TOGGLE_NEUTRAL_THREAD',
+      payload: {
+        threadId: 'thread-1',
+        userId: 'user-1',
+      },
+    };
+    // action for neutral unlike
+    const action2 = {
+      type: 'TOGGLE_NEUTRAL_THREAD',
+      payload: {
+        threadId: 'thread-1',
+        userId: 'user-2',
+      },
+    };
+
+    // next state for neutral like
+    const nextState = threadsReducer(initialState, action);
+    // expect for neutral like
+    expect(nextState).toEqual([
+      {
+        ...initialState[0],
+        upVotesBy: [],
+      },
+    ]);
+
+    // next state for neutral unlike
+    const nextState2 = threadsReducer(initialState, action2);
+    // expect for neutral unlike
+    expect(nextState2).toEqual([
+      {
+        ...initialState[0],
+        downVotesBy: [],
+      },
+    ]);
+  });
 });
