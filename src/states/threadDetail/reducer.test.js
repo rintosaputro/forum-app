069be7de9239c4cc31ffe13992c625174130ea2b
rintosaputro <saputrorinto@gmail.com>
2023-06-20 11:59:34 +0700

    feat: test RECEIVE_THREAD_DETAIL

--- src/states/threadDetail/reducer.test.js
+++ src/states/threadDetail/reducer.test.js
@@ -29,4 +29,47 @@ describe('threadDetailReducer function', () => {
 
     expect(nextState).toBe(null);
   });
+
+  it('should return the thread detail payload when given by RECEIVE_THREAD_DETAIL action', () => {
+    const initialState = [];
+    const action = {
+      type: 'RECEIVE_THREAD_DETAIL',
+      payload: {
+        threadDetail: {
+          id: 'thread-1',
+          title: 'Halo! ',
+          body: 'Bagaimana kabarmu? Semoga baik-baik saja ya.Sekali lagi saya ucapkan selamat datang semuanya!',
+          createdAt: '2023-05-29T07:54:35.746Z',
+          owner: {
+            id: 'user-1',
+            name: 'Dicoding',
+            avatar: 'https://ui-avatars.com/api/?name=Dicoding&background=random',
+          },
+          category: 'perkenalan',
+          comments: [
+            {
+              id: 'comment-1',
+              content: 'Halo!<br>Perkanalkan, nama saya Dimas.',
+              createdAt: '2023-05-29T07:59:04.689Z',
+              owner: {
+                id: 'user-2',
+                name: 'Dimas Saputra',
+                avatar: 'https://ui-avatars.com/api/?name=Dimas Saputra&background=random',
+              },
+              upVotesBy: [],
+              downVotesBy: [],
+            },
+          ],
+          upVotesBy: [
+            'user-3',
+          ],
+          downVotesBy: [],
+        },
+      },
+    };
+
+    const nextState = threadDetailReducer(initialState, action);
+
+    expect(nextState).toEqual(action.payload.threadDetail);
+  });
 });
