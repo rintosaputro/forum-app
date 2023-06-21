ae1a8db58977341ef4cc890fc84373a3fd74515d
rintosaputro <saputrorinto@gmail.com>
2023-06-21 17:14:15 +0700

    feat: onRegister event button in register

--- src/components/RegisterInput.test.jsx
+++ src/components/RegisterInput.test.jsx
@@ -12,7 +12,7 @@ import { cleanup, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import {
-  afterEach, describe, expect, it,
+  afterEach, describe, expect, it, vi,
 } from 'vitest';
 import matchers from '@testing-library/jest-dom/matchers';
 import RegisterInput from './RegisterInput';
@@ -46,5 +46,30 @@ describe('RegisterInput component', () => {
 
     await userEvent.type(passwordInput, 'passwordtest');
     expect(passwordInput).toHaveValue('passwordtest');
+  });
+
+  it('should call onRegister function when register button is clicked', async () => {
+    const mockOnRegister = vi.fn();
+    render(<RegisterInput onRegister={mockOnRegister} />);
+
+    const nameInput = await screen.getByPlaceholderText('Name');
+    await userEvent.type(nameInput, 'nametest');
+
+    const emailInput = await screen.getByPlaceholderText('Email');
+    await userEvent.type(emailInput, 'emailtest');
+
+    const passwordInput = await screen.getByPlaceholderText('Password');
+    await userEvent.type(passwordInput, 'passwordtest');
+
+    const registerButton = await screen.getByRole('button', { name: 'Register' });
+
+    await userEvent.click(registerButton);
+
+    expect(mockOnRegister).toHaveBeenCalledTimes(1);
+    expect(mockOnRegister).toHaveBeenCalledWith({
+      name: 'nametest',
+      email: 'emailtest',
+      password: 'passwordtest',
+    });
   });
 });
