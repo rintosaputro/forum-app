29d60152ddf066ae557bfa2af06cba5552e6d71c
rintosaputro <saputrorinto@gmail.com>
2023-06-21 11:58:26 +0700

    feat: test success fetch leaderboard

---
+++ src/states/leaderboards/action.test.js
@@ -0,0 +1,52 @@
+/**
+ * skenario test
+ *
+ * - asyncReceiveLeaderboards thunk
+ *  - should dispatch action correctly when data fetching success
+ *  - should dispatch action and call alert correctly when data fetching failed
+ */
+
+import {
+  afterEach, beforeEach, describe, it, vi, expect,
+} from 'vitest';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { asyncReceiveLeaderboards, receiveLeaderboardsActionCreator } from './action';
+
+const fakeLeaderboardsResponse = [
+  {
+    user: {
+      id: 'users-1',
+      name: 'John Doe',
+      email: 'john@example.com',
+      avatar: 'https://generated-image-url.jpg',
+    },
+    score: 10,
+  },
+];
+
+const fakeErrorResponse = new Error('Ups, something went wrong');
+
+describe('asyncReceiveLeaderboards thunk', () => {
+  beforeEach(() => {
+    api._getLeaderboards = api.getLeaderboards;
+  });
+
+  afterEach(() => {
+    api.getLeaderboards = api._getLeaderboards;
+
+    delete api._getLeaderboards;
+  });
+
+  it('should dispatch action correctly when data fetching success', async () => {
+    api.getLeaderboards = () => Promise.resolve(fakeLeaderboardsResponse);
+
+    const dispatch = vi.fn();
+
+    await asyncReceiveLeaderboards()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(receiveLeaderboardsActionCreator(fakeLeaderboardsResponse));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+});
