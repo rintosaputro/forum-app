ecb9ee38057b6569598b0fcd8656a06f6ec81850
rintosaputro <saputrorinto@gmail.com>
2023-06-21 15:55:36 +0700

    feat: test handle email input in register

--- src/components/RegisterInput.test.jsx
+++ src/components/RegisterInput.test.jsx
@@ -8,21 +8,35 @@
  *   - should call onRegister function when register button is clicked
  */
 
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { describe, expect, it } from 'vitest';
+import {
+  afterEach, describe, expect, it,
+} from 'vitest';
 import matchers from '@testing-library/jest-dom/matchers';
 import RegisterInput from './RegisterInput';
 
 expect.extend(matchers);
 
 describe('RegisterInput component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   it('should handle name typing correctly', async () => {
     render(<RegisterInput onRegister={() => {}} />);
     const nameInput = await screen.getByPlaceholderText('Name');
 
     await userEvent.type(nameInput, 'nametest');
     expect(nameInput).toHaveValue('nametest');
+  });
+
+  it('should handle email typing correctly', async () => {
+    render(<RegisterInput onRegister={() => {}} />);
+    const emailInput = await screen.getByPlaceholderText('Email');
+
+    await userEvent.type(emailInput, 'emailtest');
+    expect(emailInput).toHaveValue('emailtest');
   });
 });
