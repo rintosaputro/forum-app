663e0bd9b49e72495078c944937e2b32a5920f66
rintosaputro <saputrorinto@gmail.com>
2023-06-22 11:58:15 +0700

    feat: test e2e display alert when email is empty

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,13 +8,23 @@
  */
 
 describe('Login spce', () => {
-  it('should display login page correctly', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/');
+  });
 
+  it('should display login page correctly', () => {
     cy.get('input[placeholder="Email"]').should('be.visible');
     cy.get('input[placeholder="Password"]').should('be.visible');
     cy.get('button').contains(/^Login$/).should('be.visible');
     cy.get('span').contains('Don`t have an account?').should('be.visible');
     cy.get('a').contains('Register').should('be.visible');
+  });
+
+  it('should display alert when email is empty', async () => {
+    cy.get('button').contains(/^Login$/).click();
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('"email" is not allowed to be empty');
+    });
   });
 });
