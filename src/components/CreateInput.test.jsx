56cfa4e6099ba63edbf1d594d0fa6a75bd8a4582
rintosaputro <saputrorinto@gmail.com>
2023-06-22 10:21:37 +0700

    feat: test handle title input in Create

---
+++ src/components/CreateInput.test.jsx
@@ -0,0 +1,40 @@
+/**
+ * skenario testing
+ *
+ * - CreateInput component
+ *   - should handle title typing correctly
+ *   - should handle category typing correctly
+ *   - should handle body typing correctly
+ *   - should call onRegister function when register button is clicked
+ */
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import {
+  afterEach, describe, expect, it,
+} from 'vitest';
+import matchers from '@testing-library/jest-dom/matchers';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CreateInput from './CreateInput';
+
+expect.extend(matchers);
+
+describe('CreateInput component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('should handle title typing correctly', async () => {
+    render(
+      <MemoryRouter>
+        <CreateInput onPostThread={() => {}} />
+      </MemoryRouter>,
+    );
+
+    const titleInput = await screen.getByPlaceholderText('Title');
+
+    await userEvent.type(titleInput, 'titletest');
+
+    expect(titleInput).toHaveValue('titletest');
+  });
+});
